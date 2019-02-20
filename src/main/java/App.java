8f5fc7c2f8a8645b16a42461bfe399278e375446
Kayl Eubanks <kayleubanks@gmail.com>
2019-02-19 20:05:42 -0800

    Refactor deck construction into poker model and add rng based on deck size

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,35 +9,9 @@ import java.util.HashMap;
 
 public class App {
     public static void main(String[] args) {
-       Map<String, String> suits = new HashMap<String,String>();
-       suits.put("H","Hearts");
-       suits.put("D","Diamonds");
-       suits.put("C","Clubs");
-       suits.put("S","Spades");
-       Map<String, String> ranks = new HashMap<String,String>();
-       ranks.put("A", "Ace");
-       ranks.put("2", "Two");
-       ranks.put("3", "Three");
-       ranks.put("4", "Four");
-       ranks.put("5", "Five");
-       ranks.put("6", "Six");
-       ranks.put("7", "Seven");
-       ranks.put("8", "Eight");
-       ranks.put("9", "Nine");
-       ranks.put("1", "Ten");
-       ranks.put("J", "Jack");
-       ranks.put("Q", "Queen");
-       ranks.put("K", "King");
-       Map<String, String> deck = new HashMap<String, String>();
-       List cardKeys = new ArrayList<String>();
-       for(String suit: suits.keySet()) {
-           for (String rank: ranks.keySet()) {
-               String cardKey = rank + suit;
-               String cardValue = ranks.get(rank) + " of " + suits.get(suit);
-               deck.put(cardKey, cardValue);
-               cardKeys.add(cardKey);
-           }
-       }
+
+
+
 
 
     }
--- src/main/java/models/Poker.java
+++ src/main/java/models/Poker.java
@@ -2,6 +2,44 @@ package models;
 import java.util.*;
 
 public class Poker {
+    private Map deck = new HashMap();
+    private List cardKeys = new ArrayList<String>();
 
+    constructor() {
+        this.buildDeck();
+    }
 
+    private int randomNumber() {
+        return (int)(Math.random() * cardKeys.size()) + 1;
+    }
+
+    private void buildDeck() {
+        Map<String, String> suits = new HashMap<String,String>();
+        suits.put("H","Hearts");
+        suits.put("D","Diamonds");
+        suits.put("C","Clubs");
+        suits.put("S","Spades");
+        Map<String, String> ranks = new HashMap<String,String>();
+        ranks.put("A", "Ace");
+        ranks.put("2", "Two");
+        ranks.put("3", "Three");
+        ranks.put("4", "Four");
+        ranks.put("5", "Five");
+        ranks.put("6", "Six");
+        ranks.put("7", "Seven");
+        ranks.put("8", "Eight");
+        ranks.put("9", "Nine");
+        ranks.put("1", "Ten");
+        ranks.put("J", "Jack");
+        ranks.put("Q", "Queen");
+        ranks.put("K", "King");
+        for(String suit: suits.keySet()) {
+            for (String rank: ranks.keySet()) {
+                String cardKey = rank + suit;
+                String cardValue = ranks.get(rank) + " of " + suits.get(suit);
+                this.deck.put(cardKey, cardValue);
+                cardKeys.add(cardKey);
+            }
+        }
+    }
 }
